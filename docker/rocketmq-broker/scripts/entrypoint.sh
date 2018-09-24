2d1cefe8278361747e2911f001ff765756951b3d
鼠笑天 <zhuchuangang20@163.com>
2018-09-24 16:05:11 +0800

    kubernetes rocketmq脚本

--- docker/rocketmq-broker/scripts/entrypoint.sh
+++ docker/rocketmq-broker/scripts/entrypoint.sh
@@ -18,23 +18,23 @@ namesrvAddr=${NAME_SRV_ADDR}
 #在发送消息时，自动创建服务器不存在的topic，默认创建的队列数
 defaultTopicQueueNums=4
 #是否允许 Broker 自动创建Topic，建议线下开启，线上关闭
-autoCreateTopicEnable=true
+autoCreateTopicEnable=false
 #是否允许 Broker 自动创建订阅组，建议线下开启，线上关闭
-autoCreateSubscriptionGroup=true
+autoCreateSubscriptionGroup=false
 #默认不配置brokerIP1和brokerIP2时，都会根据当前网卡选择一个IP使用，当你的机器有多块网卡时，很有可能会有问题。
 #brokerIP1=10.100.50.111
 #存储路径
-storePathRootDir=/opt/rocketmq-${ROCKETMQ_VERSION}/data/store
+storePathRootDir=/opt/rocketmq/store
 #commitLog 存储路径
-storePathCommitLog=/opt/rocketmq-${ROCKETMQ_VERSION}/data/store/commitlog
+storePathCommitLog=/opt/rocketmq/store/commitlog
 #消费队列存储路径存储路径
-storePathConsumerQueue=/opt/rocketmq-${ROCKETMQ_VERSION}/data/store/consumequeue
+storePathConsumerQueue=/opt/rocketmq/store/consumequeue
 #消息索引存储路径
-storePathIndex=/opt/rocketmq-${ROCKETMQ_VERSION}/data/store/index
+storePathIndex=/opt/rocketmq/store/index
 #checkpoint 文件存储路径
-storeCheckpoint=/opt/rocketmq-${ROCKETMQ_VERSION}/data/store/checkpoint
+storeCheckpoint=/opt/rocketmq/store/checkpoint
 #abort 文件存储路径
-abortFile=/opt/rocketmq-${ROCKETMQ_VERSION}/data/store/abort
+abortFile=/opt/rocketmq/store/abort
 #删除文件时间点，默认凌晨 4点
 deleteWhen=04
 #文件保留时间，默认 48 小时
